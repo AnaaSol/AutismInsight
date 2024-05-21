af3689d7d9725a1dc64580bde3963607d372dd64
AnaaSol <anasolm26@gmail.com>
2024-05-21 09:52:30 -0300

    agrego cambios en implementación

--- modules/tpot_pipeline.py
+++ modules/tpot_pipeline.py
@@ -6,12 +6,19 @@ from sklearn.svm import LinearSVC
 from tpot.builtins import StackingEstimator
 from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
-tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
-features = tpot_data.drop('target', axis=1)
+tpot_data = pd.read_csv('data/ToddlerAutismDataset.csv')
+tpot_data = tpot_data.drop("Qchat-10-Score", axis=1)
+features = tpot_data.drop('Class/ASD Traits', axis=1)
+categorical_columns = ['Sex', 'Ethnicity', 'Jaundice', 'Family_mem_with_ASD', 'Who completed the test']
+for i in categorical_columns:
+    features[i] = label_encoder.fit_transform(features[i])
 training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'], random_state=42)
+            train_test_split(features, label_encoder.fit_transform(tpot_data['Class/ASD Traits']), random_state=42)
 
 # Average CV score on the training set was: 0.9980582524271846
 exported_pipeline = make_pipeline(
@@ -21,5 +28,5 @@ exported_pipeline = make_pipeline(
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
-exported_pipeline.fit(training_features, training_target)
-results = exported_pipeline.predict(testing_features)
+def tpot_pipeline():
+    return exported_pipeline
--- server.py
+++ server.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 #modules
 from modules.config import app, db
-from modules.model import prediction
+from modules.tpot_pipeline import tpot_pipeline
 
 #excepciones
 from sqlalchemy.orm.exc import NoResultFound 
+
+#otros
+import pandas as pd
+import numpy as np
+
+pipeline = tpot_pipeline()
+input_data = pd.DataFrame([input_features])
+input_data = np.array(input_features).reshape(1, -1)
+prediction = pipeline.predict(input_data)
+print(f"Predicted output: {prediction[0]}")
 
 with app.app_context():
     db.create_all()
